3148eb66439d7743414e29bea0c37b912d9f6a46
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-03-17 10:58:01 +0000

    aula3

--- aula3/E1.java
+++ aula3/E1.java
@@ -20,20 +20,23 @@ class Bank {
     }
 
     private Map<Integer,Account> acs = new HashMap<>();
-    private Lock l = new ReentrantLock();
+    //private Lock l = new ReentrantLock();
+    private ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
     private int nextID = 0;
 
     public int createAccount(int bal){ // temos q dar lock ao banco
         Account ac = new Account();
         ac.deposit(bal);
-        l.lock();
+        //l.lock();
+        rwl.writeLock().lock();;
         try{
             int id = nextID;
             nextID += 1;
             acs.put(id, ac);
             return id;      
         } finally {  // unlock tem de estar antes do return!!! ou fazemos este try pq evita runtimes exceptions
-            l.unlock();
+            //l.unlock();
+            rwl.writeLock().unlock();
         }
     }
 
@@ -60,14 +63,26 @@ class Bank {
     }
 
     public void deposit(int id, int val) throws InvalidAccount{
-        if (id < 0 || id >= acs.length)
+        if (id < 0 || id >= nextID)
             throw new InvalidAccount();
-        acs[id].deposit(val);
+        Account c = acs.get(id);
+        c.l.lock();
+        try{
+            c.deposit(val);
+        } finally {
+            c.l.unlock();
+        }
     }
     public void withdraw(int id, int val) throws InvalidAccount, NotEnoughFunds{
-        if (id < 0 || id >= acs.length)
+        if (id < 0 || id >= nextID)
             throw new InvalidAccount();
-        acs[id].withdraw(val);
+            Account c = acs.get(id);
+            c.l.lock();
+            try{
+                c.withdraw(val);
+            } finally {
+                c.l.unlock();
+            }
     }
 
     public int balance(int id) throws InvalidAccount{
@@ -85,30 +100,62 @@ class Bank {
     }
 
     public int totalBalance(int accounts[]) throws InvalidAccount{
+        accounts = accounts.clone();
+        Arrays.sort(accounts);
         int ret = 0;
-        for (int i = 0; i < accounts.length; i++) {
-            ret += acs[accounts[i]].balance;
+        Account[] contas = new Account[accounts.length];
+        l.lock();
+        try{
+            for (int i = 0; i < accounts.length; i++) {
+                contas[i] = acs.get(accounts[i]);
+                if (contas[i] == null)
+                    throw new InvalidAccount();
+            }
+            for (int i = 0; i< accounts.length; i++)
+                contas[i].l.lock();
+        } finally {
+            l.unlock();
         }
+        for (Account c : contas) {
+            ret += c.balance;
+            c.l.unlock();
+        }    
         return ret;
     }
 
     public void transfer(int from, int to, int val) throws InvalidAccount, NotEnoughFunds {
-        if (from < 0 || from >= acs.length || to < 0 || to >= acs.length)
+        if (from < 0 || from >= this.nextID || to < 0 || to >= this.nextID)
             throw new InvalidAccount();
-        Account acfrom = acs[from];
-        Account acto = acs[to];      // tem de estar em order de maneira a evitar deadlock
-        Account c1,c2;
-
-        if (from < to){
-            c1 = acfrom;
-            c2 = acto;
-        } else {
-            c2 = acfrom;
-            c1 = acto;
+        Account acfrom,acto;    
+        //l.lock();
+        rwl.readLock().lock();
+        try{
+            acfrom = acs.get(from);
+            acto = acs.get(to);      // tem de estar em order de maneira a evitar deadlock
+            if (acfrom == null || acto == null){
+                throw new InvalidAccount();
+            }
+            if (from < to){
+                acfrom.l.lock();
+                acto.l.lock();
+            } else {
+                acto.l.lock();
+                acfrom.l.lock();
+            }
+        } finally {
+            //l.unlock();
+            rwl.readLock().unlock();
         }
-
-        acfrom.withdraw(val);
-        acto.deposit(val);
+        try {
+            try {
+                acfrom.withdraw(val);
+            } finally {
+                acfrom.l.unlock();
+            }
+            acto.deposit(val);
+        } finally {
+            acto.l.unlock();
+        }
     }
 }
 
